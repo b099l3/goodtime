14c4cc75a3ea698a1999f81e700658842eb05e21
Iain Smith <b099l3@gmail.com>
2025-07-28 17:45:40 +0100

    some text changes on faqs

--- components/ui/faq.tsx
+++ components/ui/faq.tsx
@@ -1,8 +1,9 @@
-import { FC } from "react"
+import { FC } from "react";
+import { JSX } from "react/jsx-runtime";
 
 interface FAQItem {
-  question: string
-  answer: string | JSX.Element
+  question: string;
+  answer: string | JSX.Element;
 }
 
 const faqs: FAQItem[] = [
@@ -14,16 +15,15 @@ const faqs: FAQItem[] = [
   {
     question: "When/Where do you run?",
     answer: (
-      <ul className="list-disc pl-4 space-y-1">
-        <li>Mondays, 18:30 @civerinos_official on the portobello prom, 5km</li>
-        <li>Thursdays, 18:30 @oldeastwaytap, 5km</li>
-        <li>Sundays @traade_space, 8:00am 5km</li>
-      </ul>
+      <a className="font-bold text-primary" href="#runs">
+        See our regular runs above
+      </a>
     ),
   },
   {
     question: "Do I need sign up/pay",
-    answer: "No, just show up if you fancy it, no need to let us know if you’re coming. The runs are completely free.",
+    answer:
+      "No, just show up if you fancy it, no need to let us know if you’re coming. The runs are completely free.",
   },
   {
     question: "Do you do any longer runs/other events?",
@@ -40,28 +40,33 @@ const faqs: FAQItem[] = [
     answer:
       "Yes, right now good time tote bags can be purchased from @traade_space or @upandrunningedinburgh, for other products check the links in our bio.",
   },
-]
+];
 
 const FAQ: FC = () => {
   return (
     <section id="faq" className="py-4 pb-8 px-2 bg-muted/50">
       <div className="container mx-auto">
         <div className="text-center mb-12">
-          <h2 className="text-3xl font-bold tracking-tight sm:text-4xl mb-4">FAQs</h2>
+          <h2 className="text-3xl font-bold tracking-tight sm:text-4xl mb-4">
+            FAQs
+          </h2>
         </div>
         <div className="space-y-4 max-w-3xl mx-auto">
           {faqs.map((item, idx) => (
-            <details key={idx} className="border rounded-lg p-4 bg-white shadow">
-              <summary className="font-bold cursor-pointer">{item.question}</summary>
-              <div className="mt-2 text-muted-foreground text-base">
-                {item.answer}
-              </div>
+            <details
+              key={idx}
+              className="border rounded-lg p-4 bg-white shadow"
+            >
+              <summary className="font-bold cursor-pointer text-base">
+                {item.question}
+              </summary>
+              <div className="mt-2 text-muted-foreground ">{item.answer}</div>
             </details>
           ))}
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default FAQ
+export default FAQ;
